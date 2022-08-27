f80bbd4b6b9f6155ddf18bafef430eb187c07695
MITTHOMA-T490\MITTHOMA <mittu.thomas@oracle.com>
2022-08-28 02:22:20 +0530

    commit

--- service-a/src/main/java/com/example/helidon/micro/HelloController.java
+++ service-a/src/main/java/com/example/helidon/micro/HelloController.java
@@ -3,6 +3,8 @@ package com.example.helidon.micro;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  *
@@ -13,6 +15,17 @@ public class HelloController {
 
     @GET
     public String sayHello() {
-        return "Hello World";
+
+        InetAddress localhost = null;
+        String ip =">";
+        try {
+            localhost = InetAddress.getLocalHost();
+            ip = localhost.getHostAddress().trim();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+       System.out.println(ip);
+
+        return ip + "Hello World";
     }
 }
